295f3b34ca4c8aa12e310f8afa52969e33444628
Yu Sun <yusun@pivotal-guest-59.sf.pivotallabs.com>
2014-03-17 18:46:41 -0700

    post AngularJS pairing 2

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,16 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', function($scope, $http) {
+  	$scope.firstName = "Dilys";
+  	$scope.firstNamePlace = "first name goes here";
 
-  }])
+  	// AngularJS build in HTTP service
+  	$http.get('js/data.json').
+  	success(function(data){$scope.data = data}).error(function(err){$scope.err=err});
+
+  	//$scope.full_name = $scope.first_name + " " + $scope.last_name;
+  })
   .controller('MyCtrl2', [function() {
 
   }]);
